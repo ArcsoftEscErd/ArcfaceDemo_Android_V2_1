9a21933515389e9e210aa0401a53c6bf2c209c3d
ArcSoftEscErd <wsy9057@arcsoft.com.cn>
2019-12-23 10:29:20 +0800

    【首次上传】

---
+++ app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
@@ -0,0 +1,6 @@
+package com.arcsoft.arcfacedemo.common;
+
+public class Constants {
+    public static final String APP_ID = "官网获取的APP_ID";
+    public static final String SDK_KEY = "官网获取的SDK_KEY";
+}
